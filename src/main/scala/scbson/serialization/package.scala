e68891135f975910ccc243ed7f403f2d7a78f88b
Herr Ritschwumm <ritschwumm@djini.de>
2013-01-11 23:35:33 +0100

    updated libraries, added comments

--- src/main/scala/scbson/serialization/package.scala
+++ src/main/scala/scbson/serialization/package.scala
@@ -6,15 +6,18 @@ import scutil.lang._
 package object serialization {
 	import BSONSerializationUtil._
 	
+	/** convert values to BSON and back */
 	type BSONFormat[T]	= Bijection[T,BSONValue]
 	
+	/** create a BSONFormat from the two halves of a Bijection */
 	def BSONFormat[T](writeFunc:T=>BSONValue, readFunc:BSONValue=>T):BSONFormat[T]	= 
 			Bijection(writeFunc, readFunc)
 	
-	// TODO ugly hack
+	/** this is a bit of a hack to force a specific constructor to be used for decoding */
 	def BSONFormatSubtype[T,U<:BSONValue](writeFunc:T=>BSONValue, readFunc:U=>T):BSONFormat[T]	= 
 			BSONFormat[T](writeFunc, it => readFunc(downcast(it)))
-		
+	
+	/** delay the construction of an actual BSONFormat until it's used */
 	def BSONFormatLazy[T](sub: =>BSONFormat[T]):BSONFormat[T]	= new Bijection[T,BSONValue] {
 		lazy val delegate = sub
 		def write(t:T):BSONValue	= delegate write t
@@ -23,8 +26,11 @@ package object serialization {
 	
 	//------------------------------------------------------------------------------
 	
+	/** provide a BSONFormat for a specific value type */
 	def bsonFormat[T:BSONFormat]	= implicitly[BSONFormat[T]]
 	
+	/** encode a value into its BSON representation using an implicitly provided BSONFormat */
 	def doWrite[T:BSONFormat](it:T):BSONValue	= bsonFormat[T] write it
+	/** decode a value from its BSON representation using an implicitly provided BSONFormat */
 	def doRead[T:BSONFormat](it:BSONValue):T	= bsonFormat[T] read it
 }
